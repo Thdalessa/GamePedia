4cafc7943475d2f37dc9104a264252a4f441a87b
Thdalessa <thdalessa186@gmail.com>
2022-03-11 16:49:41 -0300

    Adds new Title to the PI, now it is called GamePedia!

--- client/src/Components/LandingPage/LandingTitle.jsx
+++ client/src/Components/LandingPage/LandingTitle.jsx
@@ -4,8 +4,8 @@ import styles from './LandingTitle.module.css';
 function LandingTitle() {
     return (
          <div className={styles.TitleContainer} >
-             <h1 className={styles.Title}>Videogame Individual Project</h1>
-             <h3 className={styles.Subtitle}>Henry's Bootcamp</h3>
+             <h1 className={styles.Title}>GamePedia Project</h1>
+             <h3 className={styles.Subtitle}>Henry's Bootcamp Individual Project</h3>
          </div>
 
     );
